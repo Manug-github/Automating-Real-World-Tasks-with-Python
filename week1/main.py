9fd3f5410121e581bf1147af3f3d5629b15669f7
Manu <manuelperezgargallo@gmail.com>
2021-02-24 01:45:37 +0100

    Pass test

--- week1/main.py
+++ week1/main.py
@@ -1,1 +1,17 @@
+#!/usr/bin/python3
+
+from PIL import Image
+import glob
+
+folder_in = "images/"
+folder_out = "/opt/icons/"
+
+ext_out = "JPEG"
+
+images = glob.glob(folder_in+"*")
+for image in images:
+    name = image[len(folder_in)+1:]
+
+    im = Image.open(image)
+    im.rotate(90).resize((128,128)).convert('RGB').save(folder_out + "/" + name, ext_out)
 
